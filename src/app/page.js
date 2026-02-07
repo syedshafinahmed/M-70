e3d3a8c576c3d81fe6b33b6475628da8524d6571
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-02-07 12:33:21 +0600

    Added Favicon and Title

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl baumans font-bold py-6">Taxi Kitchen</h1>
+      <h1 className="text-5xl baumans font-bold py-6">Yantun Khaijan</h1>
     </div>
   );
 }
