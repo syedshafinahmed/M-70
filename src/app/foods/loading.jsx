856c8c3ce609cc2b9c854c6a419b7f13fc62455e
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-02-08 13:07:11 +0600

    Updated Skeleton Loader

--- src/app/foods/loading.jsx
+++ src/app/foods/loading.jsx
@@ -4,7 +4,14 @@ import React from 'react';
 const loading = () => {
   return (
     <div>
-      <div className="h-4 w-1/4 mt-5 mb-3 py-6 rounded bg-white/10" />
+      <div className="flex items-center justify-between py-6 animate-pulse">
+        <div className="h-10 w-72 rounded bg-white/10" />
+        <div className="flex items-center gap-5">
+          <div className="h-10 w-64 rounded bg-white/10" />
+          <div className="h-10 w-10 rounded-xl bg-orange-500/30" />
+        </div>
+      </div>
+
       <div className="grid grid-cols-5 gap-6 mt-6">
         {[...Array(15)].map((_, index) => (
           <FoodCardSkeleton key={index} />
